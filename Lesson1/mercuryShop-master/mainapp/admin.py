14278147a14629d1749702aefd42f5619429bbf1
anastazia17 <stasya17kolomna@gmail.com>
2021-03-31 21:44:05 +0300

    Delete directory mercuryShop-master and add directory GeekShop **517 group**

--- Lesson1/mercuryShop-master/mainapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from mainapp.models import ProductCategory, Product, SaleSpecial
-
-admin.site.register(ProductCategory)
-admin.site.register(Product)
-admin.site.register(SaleSpecial)
-
